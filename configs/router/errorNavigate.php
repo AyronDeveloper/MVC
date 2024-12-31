357cb8efb382fd877c679a1f501ce149211dabaf
AyronDeveloper <114097887+AyronDeveloper@users.noreply.github.com>
2024-12-31 00:35:37 -0500

    fix navigate error

--- configs/router/errorNavigate.php
+++ configs/router/errorNavigate.php
@@ -3,8 +3,11 @@ require_once("./configs/router/navigate.php");
 
 class ErrorNavigate extends Navigate{
     
-    public static function error(){
-        if(self::getStatusGlobal()){
+    public static function error($redirection=null){
+        if(!empty($redirection)){
+            header("Location: ".$redirection);
+            exit;
+        }elseif(self::getStatusGlobal()){
             errorController::index();
         }
     }
